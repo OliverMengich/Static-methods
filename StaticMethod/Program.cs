e7e392cd57a816cc68162e7c6014a4a9be111488
OliverMengich <oliverkemei2000@gmail.com>
2020-03-01 19:40:49 +0300

    Static method

---
+++ StaticMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethod
+{
+    class FunctionCount
+    {
+        public static int count = 0;
+        public static void CountFunction()
+        {
+            count++;
+            Console.WriteLine("CountFunction Hass been called By the Main {0} times",count);
+        }
+        static void Main(string[] args)
+        {
+            for(int i = 0; i<10;i++)
+            {
+                FunctionCount.CountFunction();
+            }
+            Console.Read();
+         }
+    }
+
+}
